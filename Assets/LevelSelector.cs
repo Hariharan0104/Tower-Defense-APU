3ddb5fa0277c52663336577a10f4bc929920f51b
Hariharan <hariharanravichandran01@gmail.com>
2022-04-14 17:59:52 +0800

    .

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SceneFader fader;
+    public void Select(string levelName)
+    {
+        //fader.fade..(levelName)
+    }
+}
