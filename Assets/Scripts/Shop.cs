e3e411d81c29eacb1377814bca0de4d2fea295e6
Hariharan <hariharanravichandran01@gmail.com>
2022-04-21 04:26:05 +0800

    Store Towers

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,25 +4,67 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public TurretBlueprint standardTurret;
-    public TurretBlueprint anotherTurret;
+    public TurretBlueprint coin_Turret;
+    public TurretBlueprint tanker_Turret;
+    public TurretBlueprint Turret1;
+    public TurretBlueprint Turret2;
+    public TurretBlueprint Turret3;
+    public TurretBlueprint Turret4;
+    public TurretBlueprint Turret5;
+    public TurretBlueprint Turret6;
     BuildManager buildManager;
 
     void Start()
     {
         buildManager = BuildManager.instance;
     }
-
-    public void SelectStandardTurret()
+    public void coin_Turret_F()
     {
-        Debug.Log("stanbdard turret Selected");
-        buildManager.SelectTurretToBuild(standardTurret);
+        Debug.Log("coin turret Selected");
+        buildManager.SelectTurretToBuild(coin_Turret);
+    }
+    public void tanker_Turret_F()
+    {
+        Debug.Log("tanker turret Selected");
+        buildManager.SelectTurretToBuild(tanker_Turret);
     }
 
-    public void SelectAnotherTurret()
+    public void Turret_1()
     {
-        Debug.Log("Another turret Selected");
-        buildManager.SelectTurretToBuild(anotherTurret);
+        Debug.Log("Turret1 turret Selected");
+        buildManager.SelectTurretToBuild(Turret1);
     }
+
+    public void Turret_2()
+    {
+        Debug.Log("Turret2 turret Selected");
+        buildManager.SelectTurretToBuild(Turret2);
+    }
+
+    public void Turret_3()
+    {
+        Debug.Log("Turret3 turret Selected");
+        buildManager.SelectTurretToBuild(Turret3);
+    }
+
+    public void Turret_4()
+    {
+        Debug.Log("Turret4 turret Selected");
+        buildManager.SelectTurretToBuild(Turret4);
+    }
+
+    public void Turret_5()
+    {
+        Debug.Log("Turret5 turret Selected");
+        buildManager.SelectTurretToBuild(Turret5);
+    }
+
+    public void Turret_6()
+    {
+        Debug.Log("Turret6 turret Selected");
+        buildManager.SelectTurretToBuild(Turret6);
+    }
+
+ 
 
 }
