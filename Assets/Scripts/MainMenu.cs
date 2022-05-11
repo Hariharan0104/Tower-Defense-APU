2ad79063a2bc13806772940cbc220cca46643c71
Hariharan <hariharanravichandran01@gmail.com>
2022-05-12 05:26:29 +0800

    New Input System

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.InputSystem;
+
 public class MainMenu : MonoBehaviour
 {
 
