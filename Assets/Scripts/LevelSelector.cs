42ea2fa8ea85a796977aced1386688beb9481b5f
Hariharan <hariharanravichandran01@gmail.com>
2022-05-12 01:28:11 +0800

    stage levels

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     public SceneFader fader;
-    public void Select(string levelName)
+    public Button[] lvlButtons;
+
+    void Start()
     {
-        //fader.fade..(levelName)
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        for (var i = 0; i < lvlButtons.Length; i++)
+        {
+            if(i + 1 > levelReached)
+            {
+                lvlButtons[i].interactable = false;
+            }
+            
+        }
+    }
+    //use this at end of every levels
+    public void WinLevel()
+    {
+        PlayerPrefs.SetInt("levelReached", 2);
+        
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,49 +5,45 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    // public string levelToLoad = "";
-
     public SceneFader sceneFader;
-    // public void Play() {
-    //     {
-    //         sceneFader.FadeTo("Tutorial Lvl");
-    //     }
-        
-    // }
-
-    public void leave()
-        {
-            Application.Quit();
-        }
-
+    
+   
+    //UI buttons in main menu pg
     public void StartGame()
     {
         sceneFader.FadeTo("Stage Lvl");
     }
 
-    public void OpenOption()
+     public void leave()
+    {
+        Application.Quit();
+    }
+
+    public void Option()
     {
         sceneFader.FadeTo("Options");
     }
+    
+    public void How2play()
+    {
+        sceneFader.FadeTo("HowtoPlay");
+    }
+
+    public void Credit()
+    {
+        sceneFader.FadeTo("credits");
+    }
+
+
 
     public void CloseOption()
     {
         //sceneFader.FadeTo();
     }
 
-    public void How2play()
-    {
-        sceneFader.FadeTo("HowtoPlay");
-    }
-
     public void CloseHow2play()
     {
         sceneFader.FadeTo("MainMenu");
-    }
-
-    public void Credit()
-    {
-        sceneFader.FadeTo("credits");
     }
 
     public void CloseCredit()
