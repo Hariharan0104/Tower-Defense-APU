230a3f01155fb593c598b0abac64d0f3a40df81c
Hariharan <hariharanravichandran01@gmail.com>
2022-05-11 15:50:21 +0800

    Enemies movement(rotations)

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
+Quaternion enemyRotationtowaypoint;
    private Transform target;
     private int wavepointIndex = 0;
 
@@ -19,6 +20,9 @@ public class EnemyMovement : MonoBehaviour
         private void Update() {
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        enemyRotationtowaypoint = Quaternion.LookRotation(dir);
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, enemyRotationtowaypoint, Time.deltaTime * 10);
 
         if(Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
